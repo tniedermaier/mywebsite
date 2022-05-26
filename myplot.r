0eaeb176ea466a4c5e80c10151a9fad77807f747
tniedermaier <94072970+tniedermaier@users.noreply.github.com>
2022-05-26 09:31:49 +0200

    Add files via upload

--- myplot.r
+++ myplot.r
@@ -1,18 +1,11 @@
 
-#This is a slightly modified version of R's base plot() function. Its advantage is that it tells if there are NA vales in the x or y variable, and in which of them.
+myplot <- function(x=NULL, y=NULL, ...){
+if(any(is.na(x))){
+ warning("NA or NaN values detected in x variable.")
+}
 
-myplot <- function(x, y, ...){
-  if(any(is.na(x))){
-    warning("NA or NaN values detected in x variable.")
-    }
-  if(any(is.na(y))){
-      warning("NA or NaN values detected in y variable.")
-    }
-    plot(x, y, ...)
-  }
-
-## Example:
-#dat <- mtcars
-#dat$wt[4] <- NA
-#dat$mpg[8] <- NA
-#myplot(dat$wt, dat$mpg)
+if(any(is.na(y))){
+ warning("NA or NaN values detected in y variable.")
+}
+ plot(x, y, ...)
+}
